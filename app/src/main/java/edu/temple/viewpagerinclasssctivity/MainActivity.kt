4fae67edc3669fd0d42f860bb81d9169e5850393
Nicholas Rehac <tup88289@temple.edu>
2024-03-28 12:22:22 -0400

    add button adds fragments to pager

--- app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
+++ app/src/main/java/edu/temple/viewpagerinclasssctivity/MainActivity.kt
@@ -16,18 +16,23 @@ class MainActivity : AppCompatActivity() {
     private val viewPager: ViewPager2 by lazy {
         findViewById(R.id.viewPager)
     }
-
+    var numFragments = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewPager.adapter = object: FragmentStateAdapter(this) {
-            override fun getItemCount() = 10
+            override fun getItemCount() = numFragments
 
             override fun createFragment(position: Int) = TextFragment.newInstance((position + 1).toString())
 
         }
 
+        newButton.setOnClickListener {
+            numFragments++
+            (viewPager.adapter as FragmentStateAdapter).notifyDataSetChanged()
+            viewPager.setCurrentItem(numFragments - 1, true)
+        }
     }
 
 }
